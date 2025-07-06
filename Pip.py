d802966a0e08abfa9687f5d5ecbf13c52cfde94d
Pat1 <sidundanylo@gmail.com>
2025-07-06 21:24:47 +0300

    and

--- Pip.py
+++ Pip.py
@@ -6,8 +6,6 @@ def second_index(text, some_str):
     if second == -1:
         return None
     return second
-
-# Тести
 assert second_index("sims", "s") == 3, 'Test1'
 assert second_index("find the river", "e") == 12, 'Test2'
 assert second_index("hi", "h") is None, 'Test3'
